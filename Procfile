3fc51e6ec8e931cd917e3a0111c16161af0349b1
heyonju <hyeonju9705@likelion.org>
2019-07-11 00:01:50 +0900

    헤로쿠 만들기위해 푸쉬중

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Django-0704_crud-additional.wsgi --log-file -
--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\pc\desktop\장고\clone4\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
